450b4ab1907ab7ca454d555337359fa0ebe0b7a8
Ryosuke Kagabu <fuzikun.ggg52.v@outlook.jp>
2019-12-03 11:56:08 +0900

    totalMoneyが0出ない時には初めに合計した値に貯金したい数値を加算していく処理を記述

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -23,12 +23,22 @@ class MainActivity : AppCompatActivity() {
             editor.apply()
             // 保存された入力された金額を読み出し
             val savedMoney = Integer.parseInt(dataStore.getString("savedMoney", "Nothing") ?: return@setOnClickListener)
+            //　初めの1回目は0、次から保存された数値に足していく
             val firstTotalMoney = 0
             editor.putString("totalMoney", firstTotalMoney.toString())
             editor.apply()
             val lastTotalMoney = Integer.parseInt(dataStore.getString("totalMoney", firstTotalMoney.toString()).toString())
                 .plus(savedMoney)
             Toast.makeText(this, "${lastTotalMoney}円貯まりました！", Toast.LENGTH_SHORT).show()
+            when {
+              lastTotalMoney == 0 -> Toast.makeText(this, "金額を入力してください", Toast.LENGTH_SHORT).show()
+              lastTotalMoney > 0 -> {
+                  editor.putString("eternalTotalMoney", lastTotalMoney.toString())
+                  editor.apply()
+                  val eternalTotalMoney = Integer.parseInt(dataStore.getString("eternalTotalMoney", lastTotalMoney.toString()).toString())
+                  Toast.makeText(this, eternalTotalMoney.plus(lastTotalMoney).toString(), Toast.LENGTH_SHORT).show()
+              }
+            }
         }
     }
 }
